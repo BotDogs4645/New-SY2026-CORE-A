ec102060562b20fbe9e6dbdd9b9a9a9d5a2113a5
prajunaa <arjunaprabgamer@gmail.com>
2026-03-02 16:38:37 -0600

    constants updated

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -3,17 +3,17 @@ package frc.robot.subsystems.intake;
 import com.ctre.phoenix6.CANBus;
 
 public final class IntakeConstants {
-  public static final int rollerMotorCanId = 41;
+  public static final int rollerMotorCanId = 17;
   public static final CANBus rollerMotorCanBus = new CANBus("canivore");
 
-  public static final int armMotorCanID = 67;
+  public static final int armMotorCanID = 19;
   public static final CANBus armMotorCanBus = new CANBus("canivore");
 
   // for active duty cycle out, these values are the proportion of the supply voltage to apply
   public static final double rollerActiveVoltageProportion = 0.1;
   public static final double armActiveVoltageProportion = 0.1;
 
-  public static final double kRollerInRadPerSec = 67.67;
+  public static final double kRollerInRadPerSec = 30;
   public static final double kArmUpRad = 0.82;
   public static final double kArmDownRad = 0.124512;
   public static final double kArmDownRadHalf = 0.75;
